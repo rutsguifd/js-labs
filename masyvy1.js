a3bb48faf2f722078aa4dde34c9bb2f0b4063905
rutsguifd <74872948+rutsguifd@users.noreply.github.com>
2021-04-12 02:24:42 +0300

    Create masyvy1.js

---
+++ masyvy1.js
@@ -0,0 +1,43 @@
+============= zavd 0 ===============================
+function Pm (a) {
+    if (a ==12 || a ==1 || a==2 ) alert("zyma")
+    if (a ==5 || a ==3 || a==4 ) alert("vesna")
+    if (a ==8 || a ==6 || a==7 ) alert("lito")
+    if (a ==9 || a ==10 || a==11 ) alert("osin'")
+}
+
+a = parseInt(prompt())
+Pm(a);
+
+================= zavd 1 =============================
+function Pm (a) {
+    if (a ==12 || a ==1 || a==2 ) alert("zyma")
+    if (a ==5 || a ==3 || a==4 ) alert("vesna")
+    if (a ==8 || a ==6 || a==7 ) alert("lito")
+    if (a ==9 || a ==10 || a==11 ) alert("osin'")
+}
+
+a = parseInt(prompt())
+Pm(a);
+
+function Nm (b) {
+    if (a==1) alert("sicheny")
+    if (a==2) alert("lyutyi")
+    if (a==3) alert("berezeny")
+    if (a==4) alert("kviteny")
+    if (a==5) alert("traveny")
+    if (a==6) alert("cherveny")
+    if (a==7) alert("lypeny")
+    if (a==8) alert("serpeny")
+    if (a==9) alert("vereseny")
+    if (a==10) alert("zhovten")
+    if (a==11) alert("lystopad")
+    if (a==12) alert("hrudeny")
+}
+
+a = parseInt(prompt())
+Nm(a);
+
+================== zavd 2 ============================
+
+upcoming...
