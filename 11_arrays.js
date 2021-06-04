850fdf6412f8abc4b0cb6047a32cb1cf700cc3f5
rutsguifd <74872948+rutsguifd@users.noreply.github.com>
2021-06-04 22:26:15 +0300

    Update 11_arrays.js

--- 11_arrays.js
+++ 11_arrays.js
@@ -2,7 +2,6 @@
   
   let arr = [12, 32, 45, 5, 76, 7, 9]
 const a = arr.reduce((partial_sum, a) => partial_sum + a,0)/arr.length
-
 if(a>=4) console.log('vidminnyk')
 else if(3<=a<4) console.log('horoshyst')
 else console.log('dvoyoshnyk') 
@@ -11,17 +10,15 @@ else console.log('dvoyoshnyk')
 
 let arr = [13, 32, 45, 5, 76, 7, 9]
 
-arr.forEach((item, index, array) => 
-{
+arr.forEach((item) => {
     if(item<20) console.log(item+'<20') 
 });
-arr.forEach((item, index, array) => 
-{
+arr.forEach((item, index) => {
     if(index<5) console.log(item) 
 });
 console.log(arr[arr.length -1]+arr[arr.length -2])
 
-arr.sort((a,b) => b-a)
+arr.sort((a,b) => a-b)
 console.log(arr[0]+'-min')
 console.log(arr[arr.length - 1]+'-max')
 
@@ -29,8 +26,7 @@ console.log(arr[arr.length - 1]+'-max')
   
 let arr = ['sdf', 'sadf', 'adsfs', 'sddsaf', 'asd', 'Ivan', 'Ivan']
 let s = 0 ;
-arr.forEach((item, index, array) => 
-{
+arr.forEach((item) => {
     if(item==='Ivan') s++;  
 });
 console.log(s)
@@ -53,14 +49,14 @@ console.log(s)
 
 =================== zavd 1+ ===================
   
-let arr = [1243, 23432, 4234,32, 4234, 4234,4234 ,5,46, 34534, 34, 344]
+let arr = [1243, 23432, 4234, 32, 4234, 4234, 4234, 5, 46, 34534, 34, 344]
 let sum = 0;
-arr.forEach((item, index, array) => {
+arr.forEach((item, index) => {
     if (index<6) sum+=item;
 });
 console.log(sum)
 sum = 0;
-arr.forEach((item, index, array) => {
+arr.forEach((item, index) => {
     if (index>6) sum+=item;
 });
 console.log(sum)
@@ -87,6 +83,6 @@ console.log(arr.length-sum)
 let arr = [13213, 232, 3213, 4213, 235, 143, 33, 4432, 5342, 1324, 342, 3324]
 let arr1 = ['jeyj','dsh','htdfht','hgdfj','thrf','th','jgh','kru','fgdh','hgfdh','dgfh','hg']
 let s = parseInt(prompt());
-arr.forEach((item, index, array) => {
+arr.forEach((item, index) => {
     if (item<=s) console.log(arr[index] + ' ' + arr1[index]);
 });
