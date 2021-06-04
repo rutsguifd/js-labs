0861bbb5a006d476774d5f072721ac4e927e5872
rutsguifd <74872948+rutsguifd@users.noreply.github.com>
2021-06-04 22:15:41 +0300

    Update 10_funccii.js

--- 10_funccii.js
+++ 10_funccii.js
@@ -1,56 +1,56 @@
 ============= zavd 0 ===============================
+    
 function Pm (a) {
-    if (a ==12 || a ==1 || a==2 ) alert("zyma")
-    if (a ==5 || a ==3 || a==4 ) alert("vesna")
-    if (a ==8 || a ==6 || a==7 ) alert("lito")
-    if (a ==9 || a ==10 || a==11 ) alert("osin'")
+    switch(a) {
+        case 12: case 1: case 2:
+        return "zyma";
+        case 3: case 4: case 5:
+        return "vesna";
+        case 6: case 7: case 8:
+        return "lito";
+        case 9: case 10: case 11:
+        return "osin'";
+        default: return "wrong input"
+    }
 }
 
 a = parseInt(prompt())
-Pm(a);
+alert(Pm(a));
 
 ================= zavd 1 =============================
+
 function Pm (a) {
-    if (a ==12 || a ==1 || a==2 ) alert("zyma")
-    if (a ==5 || a ==3 || a==4 ) alert("vesna")
-    if (a ==8 || a ==6 || a==7 ) alert("lito")
-    if (a ==9 || a ==10 || a==11 ) alert("osin'")
+    switch(a) {
+        case 1: return "sichen'";
+        case 2: return "lyutiy";
+        case 3: return "berezen'";
+        case 4: return "kviten'";
+        case 5: return "traven'";
+        case 6: return "cherven'";
+        case 7: return "lypen'";
+        case 8: return "serpen'";
+        case 9: return "varesen'";
+        case 10: return "zhvten'";
+        case 11: return "lystopad";
+        case 12: return "hruden'";
+        default: return "wrong input"
+    }
 }
 
 a = parseInt(prompt())
-Pm(a);
-
-function Nm (b) {
-    if (a==1) alert("sicheny")
-    if (a==2) alert("lyutyi")
-    if (a==3) alert("berezeny")
-    if (a==4) alert("kviteny")
-    if (a==5) alert("traveny")
-    if (a==6) alert("cherveny")
-    if (a==7) alert("lypeny")
-    if (a==8) alert("serpeny")
-    if (a==9) alert("vereseny")
-    if (a==10) alert("zhovten")
-    if (a==11) alert("lystopad")
-    if (a==12) alert("hrudeny")
-}
-
-a = parseInt(prompt())
-Nm(a);
+alert(Pm(a));
 
 ================== zavd 2 ============================
 
 function n(a) {
-    if (0<a<22) alert("34 uroki")
-    else if (23<a<35) alert("kanikuli")
-    else if (36<a<52) alert("35 uroki")
-    else if (a>52) alert("u nas ne tak bahato tyzhniv")
-
-    
+    if (0<a<22) return("34 uroki")
+    else if (23<a<35) return("kanikuli")
+    else if (36<a<52) return("35 uroki")
+    else if (a>52) return("u nas ne tak bahato tyzhniv")
 }
     
 let a1 = parseInt(prompt())
-n(a1)
+alert(n(a1))
 
 ====================== zavd 3 =========================
 
@@ -93,14 +93,11 @@ function f1(a, b, c) {
     return count;
 }
 
-
 let a = parseInt(prompt())
 let b = parseInt(prompt())
 let c = parseInt(prompt())
 
-
 alert(f1(a, b, c))
-
 
 ============== zavd 5 ======================
     
@@ -155,8 +152,3 @@ function ph() {
     document.getElementById(img).src=arr[i];
 }
 ph();
-
-============== zavd 8 ======================
-
-    
- 
